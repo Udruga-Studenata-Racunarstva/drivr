d4dc64814ac883fd1c7e4d8c92208536fb5643cf
lperanni <lucianoperanni95@gmail.com>
2020-03-15 16:43:21 +0100

    started basic gui for event drud

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const logger = require('./shared/logger');
 const router = require('./router');
+const client = require('./gui');
 
 if (process.env.NODE_ENV !== 'production') {
   // eslint-disable-next-line global-require
@@ -19,10 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
+app.set('view engine', 'ejs');
 
 app.use('/api', requestLogger, router);
+app.use('/app', client);
 
 // Handle non-existing routes.
+app.use(express.static('public'));
 app.use((req, res) => res.status(404).end());
 
 module.exports = app;
